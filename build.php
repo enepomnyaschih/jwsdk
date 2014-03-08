76d4681b3d9731a15e09129a78fc2bf2cf0ac520
Egor Nepomnyaschih <enepomnyaschih@gmail.com>
2014-03-08 19:01:56 +0700

    #82: Updated jWidget SDK version.

--- build.php
+++ build.php
@@ -108,7 +108,7 @@ include_once 'php/Template/Manager.php';
 
 $date = date('Y-m-d H:i:s');
 JWSDK_Log::logTo('build.log', "\n\n[$date]");
-JWSDK_Log::logTo('build.log', 'Building frontend with jWidget SDK 0.4.3...');
+JWSDK_Log::logTo('build.log', 'Building frontend with jWidget SDK 0.5...');
 
 try
 {
